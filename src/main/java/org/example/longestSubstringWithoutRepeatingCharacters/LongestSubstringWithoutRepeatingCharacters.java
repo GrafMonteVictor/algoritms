ff082abe9203fdd52089132bf373302e24e26bac
grafMonteVictor <viktorpindrus@mail.ru>
2025-05-01 23:40:10 +0300

    acselerated LongestSubstringWithoutRepeatingCharacters

--- src/main/java/org/example/longestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/org/example/longestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,13 +1,14 @@
 package org.example.longestSubstringWithoutRepeatingCharacters;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
-        int cursor1 = 0, cursor2 = 0;
-        List<Character> listMax = new ArrayList<>();
-        List<Character> listTemp = new ArrayList<>();
+        int cursor1 = 0, cursor2 = 0, max = 0;
+        Set<Character> listTemp = new HashSet<>();
         if (s.length() == 0) {
             return 0;
         }
@@ -16,13 +17,10 @@ public class LongestSubstringWithoutRepeatingCharacters {
                 listTemp.add(s.charAt(cursor2));
                 cursor2++;
             } else {
-                if (listMax.size() < listTemp.size()) {
-                    listMax = new ArrayList<>(listTemp);
-                }
-                listTemp.remove(0);
-                cursor1++;
+                max = Math.max(max, listTemp.size());
+                listTemp.remove(s.charAt(cursor1++));
             }
         }
-        return Math.max(listMax.size(), listTemp.size());
+        return Math.max(max, listTemp.size());
     }
 }
